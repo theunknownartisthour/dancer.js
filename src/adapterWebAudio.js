1e415fc8504b2b261f561e8800e51f30701a455d
Alex <theunknownartisthour@gmail.com>
2015-02-09 16:17:53 -0800

    Quicker Fix of Index Error
    Fail-Fast with try{}catch{}

--- src/adapterWebAudio.js
+++ src/adapterWebAudio.js
@@ -46,12 +46,10 @@
       }
 
       this.audio.addEventListener( 'progress', function ( e ) {
-        if ( e.currentTarget.duration ) {
-          try{
-            _this.progress = e.currentTarget.seekable.end( 0 ) / e.currentTarget.duration;
-          } catch (e) {
-            _this.progress = 1;
-          }
+        try{
+          _this.progress = e.currentTarget.seekable.end( 0 ) / e.currentTarget.duration;
+        } catch (e) {
+          _this.progress = 1;
         }
       });
 
